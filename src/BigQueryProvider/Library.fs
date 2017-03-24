314ae9dbbae0f6029a3cb7b05ca3989633527b2b
Tomas Jansson <tomas.jansson@unacast.com>
2017-03-24 23:07:43 +0100

    raw analyze call

--- src/BigQueryProvider/Library.fs
+++ src/BigQueryProvider/Library.fs
@@ -1,16 +1,43 @@
 namespace BigQueryProvider
 
-/// Documentation for my library
-///
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
+module ProcessHelper = 
+  open System.Diagnostics
+
+  type ProcessResult = { exitCode : int; stdout : string; stderr : string }
+  let executeProcess exec cmdLine = 
+    let psi = ProcessStartInfo(exec, cmdLine)
+    psi.UseShellExecute <- false
+    psi.RedirectStandardOutput <- true
+    psi.RedirectStandardError <- true
+    psi.CreateNoWindow <- true        
+    let p = System.Diagnostics.Process.Start(psi) 
+    let output = System.Text.StringBuilder()
+    let error = System.Text.StringBuilder()
+    p.OutputDataReceived.Add(fun args -> output.Append(args.Data) |> ignore)
+    p.ErrorDataReceived.Add(fun args -> error.Append(args.Data) |> ignore)
+    p.BeginErrorReadLine()
+    p.BeginOutputReadLine()
+    p.WaitForExit()
+    { exitCode = p.ExitCode; stdout = output.ToString(); stderr = error.ToString() }
+
+module BigQueryAnalyze = 
+
+  let executeCmdBq = ProcessHelper.executeProcess "bq"
+
+  let analyzeQueryRaw queryStr = 
+    let cmdLine = sprintf "query --format=json --dry_run=true --use_legacy_sql=false '%s'" queryStr
+    executeCmdBq cmdLine
+
 module Library = 
-  
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+  let hello num = 
+    let query = """
+    SELECT
+  name
+  , cnt
+FROM `tomas.dflow`
+WHERE cnt = @cnt
+LIMIT 30
+"""
+    let res = BigQueryAnalyze.analyzeQueryRaw query
+    printfn "%A" res
+    42

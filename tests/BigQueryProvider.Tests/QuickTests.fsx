a8da9a81ecbdb785fefe6579534ed5646387caae
Tomas Jansson <tomas.jansson@unacast.com>
2017-03-25 21:27:14 +0100

    generated property

---
+++ tests/BigQueryProvider.Tests/QuickTests.fsx
@@ -0,0 +1,20 @@
+#r "../../src/BigQueryProvider/bin/Release/BigQueryProvider.dll"
+
+//BigQueryCommandProvider.MyProperty
+
+open BigQueryProvider.Provided
+open System
+//type X = BigQueryCommandProvider<CommandText = "tomas">
+
+//let x = BigQueryCommandProvider<"Tomas">
+//let x = new BigQueryCommandProvider<CommandText = "asdsa">()
+
+type X = BigQueryCommandProvider<"tomas", "SELECT name FROM `uc-prox-development.tomas.dflow`">
+
+let x = X("as")
+printfn "%A" (x.tomas)
+
+//type X = BigQueryProvider.
+
+//type X = BigQueryCommandProvider<"tomas">
+//let x = X()

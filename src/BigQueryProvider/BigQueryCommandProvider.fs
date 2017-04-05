e6ddeffbfb436fb037e62b3bb8d8275d8c365101
Sergey Tihon <sergey.tihon@gmail.com>
2017-04-05 16:50:13 +0300

    Fix for provided types

--- src/BigQueryProvider/BigQueryCommandProvider.fs
+++ src/BigQueryProvider/BigQueryCommandProvider.fs
@@ -11,43 +11,43 @@ open ProcessHelper
 
 [<TypeProvider>]
 [<CompilerMessageAttribute("This API supports the BigQueryProvider infrastructure and is not intended to be used directly from your code.", 101, IsHidden = true)>]
-type BigQueryCommandProvider (config: TypeProviderConfig) as this = 
+type BigQueryCommandProvider (config: TypeProviderConfig) as this =
   inherit TypeProviderForNamespaces()
   let nameSpace = this.GetType().Namespace
   let assembly = Assembly.GetExecutingAssembly()
 
-  do 
-    printfn "Assembly name: %A" assembly.FullName 
+  do
+    printfn "Assembly name: %A" assembly.FullName
   let providerType = ProvidedTypeDefinition(assembly, nameSpace, "BigQueryCommandProvider", Some typeof<obj>, HideObjectMethods = true)
 
   do
     providerType.DefineStaticParameters(
-            parameters = [ 
-                ProvidedStaticParameter("CommandText", typeof<string>) 
-            ],             
+            parameters = [
+                ProvidedStaticParameter("CommandText", typeof<string>)
+            ],
             instantiationFunction = (fun typeName [|commandText|] ->
                 let value = this.CreateRootType(typeName, unbox commandText)
                 value
-            ) 
+            )
         )
 
   do
     this.AddNamespace(nameSpace, [providerType])
 
-  member this.CreateRootType(typeName, commandText) = 
+  member this.CreateRootType(typeName, commandText) =
 
-    let rootType = ProvidedTypeDefinition(assembly, nameSpace, typeName, Some typeof<obj>, HideObjectMethods = false)
-      
-    let schema = 
+    let rootType = ProvidedTypeDefinition(assembly, nameSpace, typeName, Some typeof<obj>, HideObjectMethods = true)
+
+    let schema =
         commandText
         |> BigQueryHelper.analyzeQueryRaw
         |> (fun y -> y.stdout)
         |> SchemaHandling.Parsing.parseQueryMeta
 
     let providedOutputType = DesignTime.createOutputType providerType schema
-    
+
     DesignTime.createCommandCtors rootType
-    |> List.append (DesignTime.createExecute rootType commandText providedOutputType)
+    |> List.append (DesignTime.createExecute commandText providedOutputType)
     |> List.append ([providedOutputType])
     |> rootType.AddMembers
     rootType

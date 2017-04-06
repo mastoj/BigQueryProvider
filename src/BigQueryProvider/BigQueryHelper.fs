5f445be20d6d4ab42a595c4e79a837c2c792348d
Tomas Jansson <tomas.jansson@unacast.com>
2017-04-06 09:07:38 +0200

    get query meta from server and not hard coded

--- src/BigQueryProvider/BigQueryHelper.fs
+++ src/BigQueryProvider/BigQueryHelper.fs
@@ -3,11 +3,18 @@ module BigQueryProvider.BigQueryHelper
 let executeCmdBq = ProcessHelper.executeProcess "bq"
 
 let analyzeQueryRaw queryStr = 
-    {
-        stdout = """{"status":{"state":"DONE"},"kind":"bigquery#job","statistics":{"query":{"cacheHit":true,"statementType":"SELECT","totalBytesBilled":"0","totalBytesProcessed":"0","schema":{"fields":[{"type":"STRING","name":"name","mode":"NULLABLE"}]}},"creationTime":"1491396821408","totalBytesProcessed":"0"},"jobReference":{"projectId":"uc-prox-production"},"etag":"\"smpMas70-D1-zV2oEH0ud6qY21c/HTDNf5GMh53UFC08Uiwdsdb5vlw\"","configuration":{"query":{"createDisposition":"CREATE_IF_NEEDED","query":"SELECT \"tomas\" as name","writeDisposition":"WRITE_TRUNCATE","destinationTable":{"projectId":"uc-prox-production","tableId":"anond907caa3c3f50337ff414e7c16fa3281e4de1bcc","datasetId":"_2a855e87bf6147c55e896dcce917ee0deb1bc026"},"useLegacySql":false},"dryRun":true},"user_email":"tomas.jansson@unacast.com"}"""
-        stderr = ""
-        exitCode = 0
+    // {
+    //     stdout = """{"status":{"state":"DONE"},"kind":"bigquery#job","statistics":{"query":{"cacheHit":true,"statementType":"SELECT","totalBytesBilled":"0","totalBytesProcessed":"0","schema":{"fields":[{"type":"STRING","name":"name","mode":"NULLABLE"}]}},"creationTime":"1491396821408","totalBytesProcessed":"0"},"jobReference":{"projectId":"uc-prox-production"},"etag":"\"smpMas70-D1-zV2oEH0ud6qY21c/HTDNf5GMh53UFC08Uiwdsdb5vlw\"","configuration":{"query":{"createDisposition":"CREATE_IF_NEEDED","query":"SELECT \"tomas\" as name","writeDisposition":"WRITE_TRUNCATE","destinationTable":{"projectId":"uc-prox-production","tableId":"anond907caa3c3f50337ff414e7c16fa3281e4de1bcc","datasetId":"_2a855e87bf6147c55e896dcce917ee0deb1bc026"},"useLegacySql":false},"dryRun":true},"user_email":"tomas.jansson@unacast.com"}"""
+    //     stderr = ""
+    //     exitCode = 0
 
-    } : ProcessHelper.ProcessResult
-    // let cmdLine = sprintf "query --format=json --dry_run=true --use_legacy_sql=false '%s'" queryStr
-    // executeCmdBq cmdLine
+    // } : ProcessHelper.ProcessResult
+    let cmdLine = sprintf "query --format=json --dry_run=true --use_legacy_sql=false '%s'" queryStr
+    let res = executeCmdBq cmdLine
+    if res.exitCode <> 0
+    then 
+        let message = if res.stdout |> isNull || res.stdout = "" then res.stderr else res.stdout
+        raise (exn message)
+    else
+        printfn "%A" res
+        res
--- tests/BigQueryProvider.Tests/QuickTests.fsx
+++ tests/BigQueryProvider.Tests/QuickTests.fsx
@@ -1,26 +1,13 @@
 #r "../../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 #r "../../src/BigQueryProvider/bin/Release/BigQueryProvider.dll"
 
-//BigQueryCommandProvider.MyProperty
-
 open BigQueryProvider
 open System
-//type X = BigQueryCommandProvider<CommandText = "tomas">
 
-//let x = BigQueryCommandProvider<"Tomas">
-//let x = new BigQueryCommandProvider<CommandText = "asdsa">()
-
-//type X = BigQueryCommandProvider<"SELECT name FROM `uc-prox-development.tomas.dflow`">
-type X = BigQueryCommandProvider<"""SELECT "tomas" as name""">
+[<Literal>]
+let query = """SELECT xxx, name, "tomas" as another_name FROM `tomas.dflow`"""
+type X = BigQueryCommandProvider<query>
 let x = X()
-// let z = x.execute()
-// printfn "%A" z
-// printfn "%A" (x.execute2 true)
-//x
+
 let res = x.execute()
 printfn "%A" (res)
-//type X = BigQueryProvider.
-
-//type X = BigQueryCommandProvider<"tomas">
-//let x = X()
-
